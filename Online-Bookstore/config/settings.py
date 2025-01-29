b6685735c1b3b70f7d0f19ae976682e70cb2586a
Sam <sw.waithakasamuel@gmail.com>
2025-01-29 06:15:03 +0300

    Add Postgres db settings to the project settings

--- Online-Bookstore/config/settings.py
+++ Online-Bookstore/config/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
-}
+ }
 
 
 # Password validation
