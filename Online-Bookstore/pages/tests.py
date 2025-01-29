7a9fd4953c07b7e488281c8fe3414f920b384e18
Sam <sw.waithakasamuel@gmail.com>
2025-01-29 08:52:18 +0300

    refactor(tests): improve homepage tests with DRY principles
    - Move repeated values to class-level constants
    - Add setUp method for common request handling
    - Reuse self.response across test methods
    - Add URL resolution test
    - Standardize test method naming
    
    Technical changes:
    - Added url, view, template and text constants
    - Consolidated request handling in setUp()
    - Reduced code duplication while maintaining test coverage

--- Online-Bookstore/pages/tests.py
+++ Online-Bookstore/pages/tests.py
@@ -3,31 +3,36 @@ from django.urls import resolve, reverse
 
 from .views import HomePageView
 
-# Create your tests here.
 class HomePageTests(SimpleTestCase):
+    url = '/'
+    view = HomePageView
+    template = 'home.html'
+    expected_text = 'Welcome to our Bookstore'
+    unexpected_text = 'Hi there! I should not be on the page.'
+
+    def setUp(self):
+        self.response = self.client.get(self.url)
+
     def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_page_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, self.template)
 
     def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
+        url = reverse('home')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_contains_correct_html(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Welcome to our Bookstore')
+        self.assertContains(self.response, self.expected_text)
 
     def test_home_page_does_not_contain_incorrect_html(self):
-        response = self.client.get('/')
-        self.assertNotContains(response, 'Hi there! I should not be on the page.')
+        self.assertNotContains(self.response, self.unexpected_text)
 
     def test_home_page_url_resolves_home_page_view(self):
-        view = resolve('/')
+        view = resolve(self.url)
         self.assertEqual(
             view.func.__name__,
-            HomePageView.as_view().__name__
+            self.view.as_view().__name__
         )
