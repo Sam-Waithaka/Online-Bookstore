c7257d4aabaddf85d57eea4ccc3d53183252ad30
Sam <sw.waithakasamuel@gmail.com>
2025-01-29 07:09:41 +0300

    Add accounts app with custom user model setup
    - Created a `CustomUser` model extending Django's `AbstractUser`.
    - Registered `CustomUser` in the admin panel with a custom `UserAdmin`.
    - Added user forms for account creation and profile updates.
    - Integrated `get_user_model()` to dynamically reference the custom user model.

--- Online-Bookstore/config/settings.py
+++ Online-Bookstore/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL  ='accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
