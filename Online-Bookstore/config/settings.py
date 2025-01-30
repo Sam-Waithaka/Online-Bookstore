82fa63171a584f7fbe51ff37fe8e435a4db29adb
Sam <sw.waithakasamuel@gmail.com>
2025-01-30 14:02:08 +0300

    feat(auth): configure email-based authentication with django-allauth
    - Disable username requirement
    - Set email as authentication method
    - Make email field required
    - Enforce unique email addresses
    
    Technical changes:
    ACCOUNT_USERNAME_REQUIRED = False
    ACCOUNT_AUTHENTICATION_METHOD = 'email'
    ACCOUNT_EMAIL_REQUIRED = True
    ACCOUNT_UNIQUE_EMAIL = True

--- Online-Bookstore/config/settings.py
+++ Online-Bookstore/config/settings.py
@@ -123,6 +123,12 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
