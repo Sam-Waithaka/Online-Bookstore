b0c2a2932afcd655e1524497f215be1a44dbc03c
Sam <sw.waithakasamuel@gmail.com>
2025-01-30 12:07:01 +0300

    feat(forms): integrate crispy forms with bootstrap5
    - Add django-crispy-forms and crispy-bootstrap5 to requirements.txt
    - Configure Crispy Forms settings with Bootstrap5 template pack
    - Modify login.html and signup.html to use crispy form tags
    - Update templates to load crispy_forms_tags
    
    Technical changes:
    - Add CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
    - Add CRISPY_TEMPLATE_PACK = 'bootstrap5'
    - Add {% load crispy_forms_tags %} to auth templates
    - Update forms rendering with {{ form|crispy }}

--- Online-Bookstore/config/settings.py
+++ Online-Bookstore/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third Party Apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # Local Apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
@@ -130,6 +134,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+# Crispy Forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -138,3 +147,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
