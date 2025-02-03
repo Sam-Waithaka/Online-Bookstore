9d88b91557b007060e606de0b5befb69736ae5b6
Sam <sw.waithakasamuel@gmail.com>
2025-02-03 12:21:29 +0300

    💡 **Add book detail view and template to display individual books**
    - Added `BookDetailView` using Django’s `DetailView` to handle individual book details.
    - Created `book_detail.html` template to display book title, author, and price.
    - Updated `urls.py` to include a new route for book details using `<int:pk>/`.
    
    This update allows users to view more details about a specific book by clicking on it from the book list page.

--- Online-Bookstore/books/views.py
+++ Online-Bookstore/books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Book
@@ -8,3 +8,9 @@ class BookListView(ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'book_list'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
