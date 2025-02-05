e5a60103ad4b307bfa3cb2d6a0a4e81439a357ae
Sam <sw.waithakasamuel@gmail.com>
2025-02-05 10:42:35 +0300

    feat: Add review functionality to book details
    - Added `Review` model with a ForeignKey to `Book` and `author`.
    - Implemented inline reviews in `BookAdmin` using `ReviewInline`.
    - Displayed book reviews in the book detail template.

--- Online-Bookstore/books/admin.py
+++ Online-Bookstore/books/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
 
 # Register your models here.
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+
+
 class BookAdmin(admin.ModelAdmin):
+    inlines = (ReviewInline,)
     list_display = ('title', 'author', 'price')
     search_fields = ('title', 'author')
     list_filter = ('author',)
     ordering = ('title',)
 
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
