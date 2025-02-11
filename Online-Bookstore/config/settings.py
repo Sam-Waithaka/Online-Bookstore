b287e0c7ee11bfb9704d082d258ceaaaaed9416f
Sam <sw.waithakasamuel@gmail.com>
2025-02-11 17:37:02 +0300

    feat: Enable caching middleware and configure cache settings
    Changes:
    - Added `django.middleware.cache.UpdateCacheMiddleware` and `django.middleware.cache.FetchFromCacheMiddleware` to the middleware configuration.
    - Set caching settings:
      - `CACHE_MIDDLEWARE_ALIAS = 'default'`
      - `CACHE_MIDDLEWARE_SECONDS = 604800` (7 days)
      - `CACHE_MIDDLEWARE_KEY_PREFIX = ''`
    
    This configuration improves performance by caching responses for frequently accessed views.

--- Online-Bookstore/config/settings.py
+++ Online-Bookstore/config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware', 
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -185,3 +187,8 @@ import socket
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 604800
+CACHE_MIDDLEWARE_KEY_PREFIX = ''
